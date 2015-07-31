9b92929d997ac19328acc82fc9ccc87fdace40ab
Étienne Vallette d'Osia <dohzya@gmail.com>
2015-08-01 00:20:45 +0900

    Improve Geodecoder error

--- app/components/Geodecoder.scala
+++ app/components/Geodecoder.scala
@@ -11,17 +11,25 @@ object Geodecoder {
 
   val baseURL = "https://maps.googleapis.com/maps/api/geocode/json"
 
-  val key = Play.configuration.getString("googlemap.key").getOrElse {
+  val key = Play.configuration.getString("googlemap.key").filterNot(_.isEmpty).getOrElse {
     sys.error("Missing googlemap.key config")
   }
 
   def call(latlng: String): Future[json.JsValue] = {
+    Logger.debug(s"CALL $baseURL?latlng=$latlng&key=$key")
     ws.WS
       .url(baseURL)
       .withQueryString("latlng" -> latlng, "key" -> key)
       .get()
       .flatMap {
-        case resp if resp.status == 200 => Future.successful(resp.json)
+        case resp if resp.status == 200 =>
+          val res = resp.json
+          (res \ "status").asOpt[String] match {
+            case Some("OK") => Future.successful(res)
+            case _ =>
+              val msg = (res \ "error_message").asOpt[String].getOrElse(json.Json.stringify(res))
+              Future.failed(UnexpectedResponse(200, msg))
+          }
         case resp => Future.failed(UnexpectedResponse(resp))
       }
   }
--- app/components/package.scala
+++ app/components/package.scala
@@ -1,6 +1,8 @@
 package components
 
-case class UnexpectedResponse(status: Int, content: String) extends Exception
+case class UnexpectedResponse(status: Int, content: String) extends Exception(
+  s"Unexpected WS response: ($status) $content"
+)
 object UnexpectedResponse {
   def apply(resp: play.api.libs.ws.WSResponse): UnexpectedResponse = UnexpectedResponse(resp.status, resp.body)
 }
